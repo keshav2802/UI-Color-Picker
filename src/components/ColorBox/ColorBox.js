0f0e3547e33a559afe49c3b9f0285dba6811c23a
keshavism28 <keshavism28@gmail.com>
2020-09-23 15:10:08 +0530

    copy button changes to copied! when a color is copied

--- src/components/ColorBox/ColorBox.js
+++ src/components/ColorBox/ColorBox.js
@@ -3,6 +3,15 @@ import {CopyToClipboard} from 'react-copy-to-clipboard';
 import './ColorBox.css';
 
 class ColorBox extends Component {
+  state = {
+    isCopied: false
+  }
+
+  handleClick = () => {
+    this.setState({isCopied: true});
+    setTimeout(() => {this.setState({isCopied: false})}, 1000);
+  }
+
   render() {
     return (
       <CopyToClipboard text={this.props.background}>
@@ -11,7 +20,7 @@ class ColorBox extends Component {
           <div className="box-content">
             <span>{this.props.name}</span>
           </div>
-          <button className="copy-button">COPY</button>
+          <button className="copy-button" onClick={this.handleClick}>{this.state.isCopied ? "COPIED!" : "COPY"}</button>
         </div>
       </div>
       </CopyToClipboard>
